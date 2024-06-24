7598ccd9c97062dce12b1df8afa6b8a1d88bd71d
Tomas Hauser <142511066+Hauserman@users.noreply.github.com>
2024-06-24 15:48:32 -0300

    InvokeRepeating TERMINADO

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject[] Spawn; // Array de prefabs (esfera, cubo, triángulo)
+    public int minObjects = 5;
+    public int maxObjects = 10;
+    int cantTotal;
+    bool juegoFrenado = false;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnObjects), 0, 6f);
+    }
+
+    void Update()
+    {
+        if (juegoFrenado)
+        {
+            CancelInvoke();
+        }
+    }
+    void SpawnObjects()
+    {
+        int objectCant = Random.Range(minObjects, maxObjects + 1);
+        cantTotal += objectCant;
+        int randomObject = Random.Range(0, Spawn.Length);
+        for (int i = 0; i < objectCant; i++)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-3, 3), 20, Random.Range(-1, 5));
+            Instantiate(Spawn[randomObject], spawnPosition, Quaternion.identity);
+        }
+    }
+}
